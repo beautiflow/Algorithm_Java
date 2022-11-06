962bf20c98e2617226d443d356c71b1caa9b17f4
beautiflow <1114ny@naver.com>
2022-11-06 19:39:56 +0900

    20221106

---
+++ src/Baekjoon/Input_Output/B_10926.java
@@ -0,0 +1,15 @@
+package Baekjoon.Input_Output;
+
+import java.util.Scanner;
+
+public class B_10926 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String A = in.next();
+
+        System.out.println(A+"??!");
+
+    }
+}
+
