ab1f1c517c7e611e4b2432691c515db83b52c443
beautiflow <1114ny@naver.com>
2022-11-20 21:08:21 +0900

    20221120

---
+++ src/Baekjoon/For_While/B_11022.java
@@ -0,0 +1,18 @@
+package Baekjoon.For_While;
+
+import java.util.Scanner;
+
+public class B_11022 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i = 1; i<=T; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A+B));
+        }
+    }
+}
