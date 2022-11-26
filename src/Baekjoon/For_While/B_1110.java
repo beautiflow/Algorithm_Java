87362b589d6d298be370387fd65ceb6786e4ed0e
beautiflow <1114ny@naver.com>
2022-11-26 23:48:23 +0900

    20221126

---
+++ src/Baekjoon/For_While/B_1110.java
@@ -0,0 +1,36 @@
+package Baekjoon.For_While;
+
+import java.util.Scanner;
+
+public class B_1110 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+
+        
+
+
+
+
+
+    }
+}
+
+
+/*
+*   # 풀이
+*   1. x의 십의 자리를 q, 일의 자리를 r이라고 할 때, q + r을 한 sum을 구한다.
+*   2. x = r * 10 + sum % 10 으로 초기화해 준다.
+*   3. x와 N이 다시 같아지는 순간까지 위 2가지 작업을 반복한다.
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* */
