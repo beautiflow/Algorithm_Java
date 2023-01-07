50f954f23ea8df709e7bf80730ffaf8698e81a11
beautiflow <1114ny@naver.com>
2023-01-07 14:04:42 +0900

    20230106

---
+++ src/solved_ac/class1/B_11654.java
@@ -0,0 +1,13 @@
+package solved_ac.class1;
+
+import java.io.IOException;
+
+public class B_11654 {
+    public static void main(String[] args) throws IOException {
+
+        int a = System.in.read();
+        System.out.println(a);
+
+
+    }
+}
