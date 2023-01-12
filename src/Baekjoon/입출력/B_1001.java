1f07a3b4a5378bcfc1bb9ae5fc91a251e1d32a21
beautiflow <1114ny@naver.com>
2023-01-12 20:00:30 +0900

    feat: 패키지 이동

--- src/Baekjoon/입출력/B_1001.java
+++ src/Baekjoon/입출력/B_1001.java
@@ -1,14 +1,14 @@
-package Baekjoon.Input_Output;
+package Baekjoon.입출력;
 
 import java.util.Scanner;
 
-public class B_1000 {
+public class B_1001 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         int A = in.nextInt();
         int B = in.nextInt();
 
-        System.out.println(A+B);
+        System.out.println(A-B);
     }
 }
--- src/Baekjoon/입출력/B_2557.java
+++ src/Baekjoon/입출력/B_2557.java
@@ -1,4 +1,4 @@
-package Baekjoon.Input_Output;
+package Baekjoon.입출력;
 
 public class B_2557 {
     public static void main(String[] args) {
