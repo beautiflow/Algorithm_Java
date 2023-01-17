3ced8c262f2b3874ebb721460872cf7cce46e885
beautiflow <1114ny@naver.com>
2023-01-18 08:21:57 +0900

    20230118

---
+++ src/solved_ac/Bronze/B_5337.java
@@ -0,0 +1,9 @@
+package solved_ac.Bronze;
+
+public class B_5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
