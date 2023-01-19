abcea78fb3c6f8f02ad719e12db3c1ee8da75223
beautiflow <1114ny@naver.com>
2023-01-19 20:14:53 +0900

    20230119

---
+++ src/solved_ac/Bronze/B_1264.java
@@ -0,0 +1,20 @@
+package solved_ac.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_1264 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String S = st.nextToken();
+
+
+
+
+    }
+
+}
