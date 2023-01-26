be777636c96f6f570c1232ad75d9cc6dae5d1431
beautiflow <1114ny@naver.com>
2023-01-26 10:41:25 +0900

    20230126

--- src/solved_ac/Bronze/B_2440.java
+++ src/solved_ac/Bronze/B_2440.java
@@ -1,18 +1,17 @@
 package solved_ac.Bronze;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
-public class B_2440 {
+public class B_2440{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < N; i++){
-            for(int j = 5 ; i < j; j--) {
+            for(int j = N; i < j; j--){
                 System.out.print("*");
             }
             System.out.println();
