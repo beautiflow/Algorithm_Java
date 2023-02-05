a3b4881f31e9b8e0c3a88a6bb490f9031dcaeaa4
beautiflow <1114ny@naver.com>
2023-02-05 16:54:10 +0900

    20230205

---
+++ src/solved_ac/Bronze/B_2338.java
@@ -0,0 +1,17 @@
+package solved_ac.Bronze;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B_2338 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+
+        System.out.println(A.add(B));
+        System.out.println(A.subtract(B));
+        System.out.println(A.multiply(B));
+    }
+}
