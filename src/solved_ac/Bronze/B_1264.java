82b55627635c3ec94576107eb5dc9b42915ed5ac
beautiflow <1114ny@naver.com>
2023-02-05 20:42:23 +0900

    20230205

--- src/solved_ac/Bronze/B_1264.java
+++ src/solved_ac/Bronze/B_1264.java
@@ -3,18 +3,26 @@ package solved_ac.Bronze;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class B_1264 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String S = st.nextToken();
-
-
-
-
+        while(true){
+            String str = br.readLine();
+            if(str.equals('#')){
+                break;
+            }
+            str = str.toLowerCase();
+            int count = 0;
+            int i = 0;
+            for(; i < str.length(); i++){
+                char t = str.charAt(i);
+                if(t == 'a' || t == 'e' || t == 'i'|| t == 'o' || t == 'u' ){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
     }
-
 }
