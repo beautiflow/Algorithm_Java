c0e42574253b9b163fe51784830a9af12810d88f
beautiflow <1114ny@naver.com>
2023-02-07 07:58:11 +0900

    20230207

---
+++ src/solved_ac/Bronze/B_1085.java
@@ -0,0 +1,19 @@
+package solved_ac.Bronze;
+
+import java.util.Scanner;
+
+public class B_1085 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int X = Math.min(x, (w - x));
+        int Y = Math.min(y, (h - y));
+
+        System.out.println(Math.min(X, Y));
+    }
+}
