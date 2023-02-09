99b6015acc9544d371dda4aa6426d6b3b02f6410
beautiflow <1114ny@naver.com>
2023-02-09 21:13:54 +0900

    20230209

---
+++ src/solved_ac/Bronze/B_5543.java
@@ -0,0 +1,29 @@
+package solved_ac.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_5543 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int burger = 2001;
+        int drink = 2001;
+
+        for(int i = 0; i < 3; i++){
+            int value = Integer.parseInt(br.readLine());
+            if(value < burger){
+                burger = value;
+            }
+        }
+
+        for(int i = 0; i < 2; i++){
+            int value = Integer.parseInt(br.readLine());
+            if (value < drink){
+                drink = value;
+            }
+        }
+        System.out.println(burger + drink -50);
+    }
+}
