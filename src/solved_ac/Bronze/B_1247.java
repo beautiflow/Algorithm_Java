417aa7bb205be7004d9ce3bc36a014c33c21e22b
beautiflow <1114ny@naver.com>
2023-02-11 11:14:43 +0900

    20230211

---
+++ src/solved_ac/Bronze/B_1247.java
@@ -0,0 +1,31 @@
+package solved_ac.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class B_1247 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int i = 0; i < 3; i++){
+            BigInteger S = new BigInteger("0");
+            int N = Integer.parseInt(br.readLine());
+
+            for(int j = 0; j < N; j++){
+                BigInteger M = new BigInteger(br.readLine());
+                S = S.add(M);
+            }
+
+            if(S.compareTo(BigInteger.ZERO) == -1){
+                System.out.println("-");
+            } else if (S.compareTo(BigInteger.ZERO) == 1) {
+                System.out.println("+");
+            }
+            else {
+                System.out.println("0");
+            }
+        }
+    }
+}
