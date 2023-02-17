e2f7ff0996b6393c527249734173953e05dbc361
beautiflow <1114ny@naver.com>
2023-02-17 19:55:39 +0900

    20230217

---
+++ src/solved_ac/Bronze/B_10039.java
@@ -0,0 +1,22 @@
+package solved_ac.Bronze;
+
+import java.util.Scanner;
+
+public class B_10039 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] score = new int[5];
+        int sum = 0;
+
+        for(int i = 0; i < 5; i++){
+            score[i] = sc.nextInt();
+
+            if(score[i] < 40){
+                score[i] = 40;
+            }
+
+            sum += score[i];
+        }
+        System.out.println(sum/5);
+    }
+}
