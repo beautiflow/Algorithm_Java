918e0ed99dc384bf263dc225fef1e3a9f28f0ba4
beautiflow <1114ny@naver.com>
2023-02-27 21:35:58 +0900

    20230227

---
+++ src/solved_ac/Bronze/B_2743.java
@@ -0,0 +1,15 @@
+package solved_ac.Bronze;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class B_2743 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(str.length());
+
+    }
+}
