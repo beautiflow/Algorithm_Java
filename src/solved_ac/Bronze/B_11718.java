c6a36ff7122b6ebca8e6dbb7903acf36c33f821e
beautiflow <1114ny@naver.com>
2023-02-28 14:33:25 +0900

    20230228

---
+++ src/solved_ac/Bronze/B_11718.java
@@ -0,0 +1,14 @@
+package solved_ac.Bronze;
+
+import java.util.Scanner;
+
+public class B_11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()){
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+    }
+}
