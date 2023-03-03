508b0273544010efec14f77a43e06ac9e1fb3dc5
beautiflow <1114ny@naver.com>
2023-03-03 13:36:59 +0900

    20230303

---
+++ src/solved_ac/Sliver/B_11047.java
@@ -0,0 +1,34 @@
+package solved_ac.Sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[N];
+
+        for(int i = 0; i < coin.length; i++){
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+
+        for(int i = N-1; i >= 0; i--){
+
+            if(coin[i] <= K){
+                count += (K / coin[i]);
+                K = K % coin[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
