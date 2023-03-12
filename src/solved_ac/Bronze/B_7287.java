7cdf38e5f40468786d66b8306875474c443133ea
beautiflow <1114ny@naver.com>
2023-03-12 21:55:59 +0900

    20230312

---
+++ src/solved_ac/Bronze/B_7287.java
@@ -0,0 +1,8 @@
+package solved_ac.Bronze;
+
+public class B_7287 {
+    public static void main(String[] args) {
+        System.out.println("158");
+        System.out.println("beautiflow");
+    }
+}
