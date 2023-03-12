83d8042dae8e7596be575003d18f69e9e7e5641d
beautiflow <1114ny@naver.com>
2023-03-12 21:55:28 +0900

    20230312

---
+++ src/solved_ac/Bronze/B_2420.java
@@ -0,0 +1,14 @@
+package solved_ac.Bronze;
+
+import java.util.Scanner;
+
+public class B_2420 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+
+        System.out.println(Math.abs(n-m));
+    }
+}
