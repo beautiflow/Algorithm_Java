e513a40eb89ac63328b6f24339944d97874d516a
beautiflow <1114ny@naver.com>
2023-03-12 17:27:16 +0900

    20230312

---
+++ src/solved_ac/Bronze/B_1924.java
@@ -0,0 +1,29 @@
+package solved_ac.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_1924 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int[] months = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] days = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+
+        int today = 0;
+
+        for(int i = 0; i < x; i++){
+            today += months[i];
+        }
+
+        today += y-1;
+
+        System.out.println(days[today%7]);
+    }
+}
