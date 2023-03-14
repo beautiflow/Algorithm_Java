a2015140542f570251abb30bc9d69073482099f9
beautiflow <1114ny@naver.com>
2023-03-14 17:33:55 +0900

    20230314

---
+++ src/solved_ac/Bronze/B_10798.java
@@ -0,0 +1,30 @@
+package solved_ac.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_10798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        char[][] arr = new char[5][15];
+
+        for(int i = 0 ; i < 5; i++){
+            String str = br.readLine();
+            for(int j = 0; j < str.length(); j++){
+                arr[i][j] = str.charAt(j);
+            }
+        }
+        for(int i = 0 ; i < 15; i++){
+            for(int j = 0; j < 5; j++){
+               if( arr[j][i] != '\0'){
+                   sb.append(arr[j][i]);
+               }
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
