2080d8e50d89ef398aa56e0f9d2dc5c3f820aadf
beautiflow <1114ny@naver.com>
2023-03-15 12:46:45 +0900

    20230315

---
+++ src/solved_ac/Sliver/B_10815.java
@@ -0,0 +1,34 @@
+package solved_ac.Sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class B_10815 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> set = new HashSet<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++){
+            int num = Integer.parseInt(st.nextToken());
+
+            if(set.contains(num)){
+                System.out.print("1" + " ");
+            }
+            else {
+                System.out.print("0" + " ");
+            }
+        }
+    }
+}
