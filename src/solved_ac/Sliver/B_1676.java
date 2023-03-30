65af9d2351a4192f2fd90886c72cb3cf7eaa8c9f
beautiflow <1114ny@naver.com>
2023-03-30 13:07:59 +0900

    20230330

---
+++ src/solved_ac/Sliver/B_1676.java
@@ -0,0 +1,20 @@
+package solved_ac.Sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while (N >= 5){
+            count += N / 5;
+            N /= 5;
+        }
+        System.out.println(count);
+    }
+}
