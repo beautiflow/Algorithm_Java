666fc7529f2e624ba77e5411a306a3d57ed51717
beautiflow <1114ny@naver.com>
2023-04-09 21:16:23 +0900

    20230409

---
+++ src/solved_ac/Sliver/B_17413.java
@@ -0,0 +1,53 @@
+package solved_ac.Sliver;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class B_17413{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        boolean in = false;
+
+        for(int i = 0; i < str.length(); i++){
+
+            if(str.charAt(i) == '<'){
+                in = true;
+
+                while (!stack.isEmpty()){
+                    System.out.print(stack.pop());
+                }
+                System.out.print(str.charAt(i));
+            }
+            else if(str.charAt(i) == '>'){
+                in = false;
+                System.out.print(str.charAt(i));
+            }
+
+            else if(in){
+                System.out.print(str.charAt(i));
+            }
+            else if(!in){
+                if(str.charAt(i) == ' '){
+                    while (!stack.isEmpty()){
+                        System.out.print(stack.pop());
+                    }
+                    System.out.print(str.charAt(i));
+                }
+                else {
+                    stack.push(str.charAt(i));
+                }
+            }
+        }
+        while (!stack.isEmpty()){
+            System.out.print(stack.pop());
+        }
+
+    }
+}
