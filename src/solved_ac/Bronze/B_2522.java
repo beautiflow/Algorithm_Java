b7a1c2e99e3580620140889ae177757133187120
beautiflow <1114ny@naver.com>
2023-04-21 12:14:26 +0900

    20230421

--- src/solved_ac/Bronze/B_2522.java
+++ src/solved_ac/Bronze/B_2522.java
@@ -10,22 +10,22 @@ public class B_2522 {
 
         int N = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i <=N ; i++){
+        for (int i = 1; i <= N; i++) {
 
-            for(int j = 1; j<= N-i; j++){
+            for (int j = 1; j <= N - i; j++) {
                 System.out.print(" ");
             }
-            for(int j = 1; j <= i; j++){
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for(int i = 1; i <= N-1; i++){
+        for (int i = 1; i <= N - 1; i++) {
 
-            for(int j = 1; j <= i; j++){
+            for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
             }
-            for(int j = 1; j <= N-i; j++){
+            for (int j = 1; j <= N - i; j++) {
                 System.out.print("*");
             }
             System.out.println();
