07c9a1ff116ec37c22ea7054df2fff3b44c81f21
beautiflow <1114ny@naver.com>
2023-04-21 12:14:12 +0900

    20230421

---
+++ src/solved_ac/Bronze/B_2522.java
@@ -0,0 +1,34 @@
+package solved_ac.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_2522 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <=N ; i++){
+
+            for(int j = 1; j<= N-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i <= N-1; i++){
+
+            for(int j = 1; j <= i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= N-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
