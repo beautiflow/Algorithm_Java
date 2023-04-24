0f5ffc3d8d170a0373d5e73d6fe8c5f4426b114a
beautiflow <1114ny@naver.com>
2023-04-24 23:15:41 +0900

    20230424

---
+++ src/solved_ac/Bronze/B_2393.java
@@ -0,0 +1,10 @@
+package solved_ac.Bronze;
+
+public class B_2393 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+    }
+}
