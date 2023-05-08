fe322e1ad6acfce42905c7ccc73c349661c6d75d
beautiflow <1114ny@naver.com>
2023-05-08 17:44:56 +0900

    20230508

---
+++ src/SamsungSW/d3/S_1208.java
@@ -0,0 +1,37 @@
+package SamsungSW.d3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S_1208 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int tc = 1; tc <= 10; tc++) {
+
+            int dump = Integer.parseInt(br.readLine());
+
+            int[] box = new int[100];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < box.length; i++) {
+                box[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(box);
+
+
+            for (int i = 0; i < dump; i++) {
+                box[0]++;
+                box[99]--;
+                Arrays.sort(box);
+            }
+            int gap = box[99] - box[0];
+
+            System.out.println("#"+tc+" "+gap);
+        }
+    }
+}
