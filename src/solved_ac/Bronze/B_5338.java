056f5175285b180d8c9ed821d7538a9c61f75213
beautiflow <1114ny@naver.com>
2023-05-20 10:34:41 +0900

    20230520

---
+++ src/solved_ac/Bronze/B_5338.java
@@ -0,0 +1,14 @@
+package solved_ac.Bronze;
+
+public class B_5338 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("       _.-;;-._").append('\n');
+        sb.append("'-..-'|   ||   |").append('\n');
+        sb.append("'-..-'|_.-;;-._|").append('\n');
+        sb.append("'-..-'|   ||   |").append('\n');
+        sb.append("'-..-'|_.-''-._|").append('\n');
+
+        System.out.println(sb);
+    }
+}
