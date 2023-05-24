951604ee681a284abd5b9a2c2c85beb39b845fb6
beautiflow <1114ny@naver.com>
2023-05-24 21:46:03 +0900

    20230524

---
+++ src/solved_ac/Bronze/B_1292.java
@@ -0,0 +1,34 @@
+package solved_ac.Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_1292 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[1002];
+        int count = 1;
+
+        for(int i = 1; i <= 1000; i++){
+            for(int j = 0; j < i; j++){
+                if(count == 1001){
+                    break;
+                }
+                arr[count] = i;
+                count++;
+            }
+        }
+        int sum = 0;
+
+        for(int i = A; i <= B; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
