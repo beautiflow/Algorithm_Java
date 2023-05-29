086d982e4f8294364daf0ee4371315f9c041c154
beautiflow <1114ny@naver.com>
2023-05-29 18:33:13 +0900

    20230529

---
+++ src/solved_ac/Bronze/B_17388.java
@@ -0,0 +1,36 @@
+package solved_ac.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_17388 {
+        public static void main(String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
+
+            int sum = S + K + Y;
+            int min = Math.min(S, K);
+            min = Math.min(min, Y);
+
+            if(sum >= 100) {
+                System.out.println("OK");
+            }else {
+                if(min == S) {
+                    System.out.println("Soongsil");
+                }else if(min == K) {
+                    System.out.println("Korea");
+                }else {
+                    System.out.println("Hanyang");
+                }
+            }
+
+        }
+
+    }
+
+
