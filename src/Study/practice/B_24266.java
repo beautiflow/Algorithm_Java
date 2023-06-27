fe7a1a1014d33bc472234934863c4144af92fd2f
beautiflow <1114ny@naver.com>
2023-06-27 09:28:22 +0900

    20230627

---
+++ src/Study/practice/B_24266.java
@@ -0,0 +1,19 @@
+package Study.practice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class B_24266 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger N = new BigInteger(br.readLine());
+
+        BigInteger multi = N.multiply(N);
+        BigInteger total = multi.multiply(N);
+
+        System.out.println(total);
+        System.out.println(3);
+    }
+}
