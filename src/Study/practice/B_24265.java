b1275e46a5b6e2b2455f8a1ced79dd89df860e6d
beautiflow <1114ny@naver.com>
2023-07-02 21:23:07 +0900

    20230702

---
+++ src/Study/practice/B_24265.java
@@ -0,0 +1,20 @@
+package Study.practice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B_24265 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long N = Long.parseLong(br.readLine());
+        long sum = 0;
+
+        for(int i = 1; i < N; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+        System.out.println(2);
+
+    }
+}
