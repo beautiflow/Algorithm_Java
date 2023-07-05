e88e4d4410e088d7a9d14abe89293849a72ae50e
beautiflow <1114ny@naver.com>
2023-07-05 19:20:12 +0900

    20230705

---
+++ src/Study/practice2/B_2847.java
@@ -0,0 +1,26 @@
+package Study.practice2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B_2847 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] score = new int[N];
+
+        for(int i = 0 ; i < score.length; i++){
+            score[i] = Integer.parseInt(br.readLine());
+        }
+        int count = 0;
+
+        for(int i = N-1; i > 0; i--){
+            while (score[i] <= score[i-1]){
+                count++;
+                score[i-1] = score[i-1]-1;
+            }
+        }
+        System.out.println(count);
+    }
+}
