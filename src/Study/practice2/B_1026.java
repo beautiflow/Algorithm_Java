10efb283187e89b8cae1185980134f1aa4cc1ccc
beautiflow <1114ny@naver.com>
2023-07-05 16:29:21 +0900

    20230705

---
+++ src/Study/practice2/B_1026.java
@@ -0,0 +1,34 @@
+package Study.practice2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B_1026 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+            B[i] = Integer.parseInt(st1.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int sum = 0;
+
+        for(int i = 0; i < A.length; i++){
+            sum += A[i]*B[N-i-1];
+        }
+        System.out.println(sum);
+    }
+}
