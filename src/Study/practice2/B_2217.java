cf9b9be8eb777e3725a191d3bb25e529874646a4
beautiflow <1114ny@naver.com>
2023-07-07 23:10:38 +0900

    20230707

---
+++ src/Study/practice2/B_2217.java
@@ -0,0 +1,31 @@
+package Study.practice2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B_2217 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int lope[] = new int[N];
+
+        for(int i = 0; i < N; i++){
+            lope[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(lope);
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < N; i++){
+            lope[i] = lope[i] * (N-i);
+            if(max < lope[i]){
+                max = lope[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
