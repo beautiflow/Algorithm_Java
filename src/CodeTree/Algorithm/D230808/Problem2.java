deca4ed7e8c7cea8f126efe746ac9df2cfea25d9
beautiflow <1114ny@naver.com>
2023-08-09 07:21:23 +0900

    20230808

---
+++ src/CodeTree/Algorithm/D230808/Problem2.java
@@ -0,0 +1,26 @@
+package CodeTree.Algorithm.D230808;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Problem2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] square = new int[N][N];
+        int count = 1;
+
+        for(int i = 0 ;i < N; i++){
+            for(int j = 0; j < N; j++){
+                square[i][j] = count++;
+                System.out.print(square[i][j]+" ");
+                if(count == 10){
+                    count = 1;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
