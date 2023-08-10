a510d682fef0e22e7a7bb43589099e8612880fcb
beautiflow <1114ny@naver.com>
2023-08-10 22:59:11 +0900

    20230810

---
+++ src/CodeTree/Algorithm/D230809/YesNo.java
@@ -0,0 +1,29 @@
+package CodeTree.Algorithm.D230809;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class YesNo {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if(correct(N)){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean correct(int N){
+        int num = N;
+        int sum = 0;
+        for(int i = 0; i < 2; i++){
+            sum += N%10;
+            N = num/10;
+        }
+
+        return num%2==0 && sum%5==0;
+    }
+}
