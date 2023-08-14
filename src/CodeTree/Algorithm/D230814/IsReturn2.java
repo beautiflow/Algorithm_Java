275f035b29324768ae153aaa666c7698896dab7a
beautiflow <1114ny@naver.com>
2023-08-14 09:20:07 +0900

    20230814

---
+++ src/CodeTree/Algorithm/D230814/IsReturn2.java
@@ -0,0 +1,25 @@
+package CodeTree.Algorithm.D230814;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class IsReturn2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+
+        System.out.println(f(x));
+    }
+
+    public static int f(int x) {
+        int cnt = 0;
+        while (x % 2 == 0) {
+            x /= 2;
+            cnt++;
+        }
+        return cnt;
+    }
+
+}
+
