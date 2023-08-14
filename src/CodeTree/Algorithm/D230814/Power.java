6815f7995d6528cc62d2e17eb226dbea75d5d9ec
beautiflow <1114ny@naver.com>
2023-08-14 21:37:01 +0900

    20230814

---
+++ src/CodeTree/Algorithm/D230814/Power.java
@@ -0,0 +1,22 @@
+package CodeTree.Algorithm.D230814;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Power {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int result = power(a, b);
+        System.out.println(result);
+    }
+
+    public static int power(int a, int b){
+        return (int)Math.pow(a, b);
+    }
+}
