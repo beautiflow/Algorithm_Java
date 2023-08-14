6c49f3e9d20b865155ee80628a9f647a2c4a9529
beautiflow <1114ny@naver.com>
2023-08-14 09:56:22 +0900

    20230814

---
+++ src/CodeTree/Algorithm/D230814/IsReturn4.java
@@ -0,0 +1,30 @@
+package CodeTree.Algorithm.D230814;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class IsReturn4 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        System.out.println(Lee(a)+Lee(b)+Lee(c)+Lee(d));
+    }
+
+    public static int Lee(int x)
+    {
+        int last = 10;
+        while (x > 0){
+            if (x % 10 > last) return 0;
+            last = x % 10;
+            x /= 10;
+        }
+        return 1;
+    }
+}
