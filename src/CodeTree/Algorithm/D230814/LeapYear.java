0e224ec673c027320605ce426381fe68b3399893
beautiflow <1114ny@naver.com>
2023-08-14 09:20:18 +0900

    20230814

---
+++ src/CodeTree/Algorithm/D230814/LeapYear.java
@@ -0,0 +1,28 @@
+package CodeTree.Algorithm.D230814;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class LeapYear {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        if(leapYear(num)){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+
+
+    }
+
+    public static boolean leapYear(int num){
+        if(num%4==0 && num%100!=0 ||num%400==0){
+            return true;
+        }
+        return false;
+    }
+}
