800fdeb2b42dbb53f63e9d7a3a9a4a37ddb94290
beautiflow <1114ny@naver.com>
2023-08-17 07:15:46 +0900

    20230817

---
+++ src/CodeTree/Algorithm/D230817/IsReturn18.java
@@ -0,0 +1,31 @@
+package CodeTree.Algorithm.D230817;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+public class IsReturn18 {
+    static int[] p = new int[]{1, 3, 7, 8, 4, 3, 5, 2, 9, 2};
+
+    public static void main(String[] args) {
+
+        System.out.println(f(0, 9, 5, 6));
+
+
+    }
+
+    public static int g(int a, int b, int x){
+        int i, c = 0;
+        for(i = a; i <= b; i++) if (p[i] <= x) c++;
+        return c;
+    }
+
+    public static int f(int a, int b, int x, int k) {
+        int mid, lc;
+        if (a == b) return p[a];
+        mid = (a + b) / 2;
+        lc=g(a,mid,x);
+        if (lc >= k) return f(a, mid, x, k);
+        else return f(mid + 1, b, x, k - lc);
+    }
+}
