ba27178279ff34f27cdeeb012491004efbb3083d
beautiflow <1114ny@naver.com>
2023-09-29 02:42:53 +0900

    20230929

---
+++ src/leetCode/Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,27 @@
+package leetCode;
+
+public class Best_Time_to_Buy_and_Sell_Stock {
+    public static void main(String[] args) {
+        int[] prices = {7,6,4,3,1};
+
+        int result = maxProfit(prices);
+        System.out.println(result);
+    }
+    public static int maxProfit(int[] prices) {
+        int profitMin = Integer.MAX_VALUE;
+        int profitMax = Integer.MIN_VALUE;
+
+        for(int i = 0; i < prices.length; i++) {
+            if (prices[i] < profitMin) {
+                profitMin = prices[i];
+            }
+            profitMax = Math.max(profitMax, prices[i] - profitMin);
+        }
+        if(profitMax > 0){
+            return profitMax;
+        }
+        else {
+            return 0;
+        }
+    }
+}
