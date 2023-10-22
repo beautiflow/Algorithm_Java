e8cfdd31dccd2ada6b89c0199225ad0ce5715383
beautiflow <1114ny@naver.com>
2023-10-22 23:32:24 +0900

    [Baekjoon] B_7785

--- src/solved_ac/Sliver/B_7785.java
+++ src/solved_ac/Sliver/B_7785.java
@@ -1,5 +1,41 @@
 package solved_ac.Sliver;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class B_7785  {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Set<String> inCompany = new HashSet<String>();
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer  st = new StringTokenizer(br.readLine());
+
+            String name = st.nextToken();
+            String log = st.nextToken();
+
+            if(log.equals("enter")){
+                inCompany.add(name);
+            }
+            else if (log.equals("leave")) {
+                inCompany.remove(name);
+            }
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        Iterator<String> iterator = inCompany.iterator();
+        while (iterator.hasNext()){
+            list.add(iterator.next());
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        for(String name : list){
+            System.out.println(name);
+        }
+    }
 }
 
