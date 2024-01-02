7911fd61d4c212109aecaacca2b8933e91ca0cb7
beautiflow <1114ny@naver.com>
2024-01-02 21:02:21 +0900

    [inflearn] 01-02

---
+++ src/Inflearn/ch01/SpellChange.java
@@ -0,0 +1,27 @@
+package Inflearn.ch01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SpellChange {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch == Character.toLowerCase(ch)){
+                sb.append(Character.toUpperCase(ch));
+            }
+            else{
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+        System.out.println(sb);
+    }
+
+}
