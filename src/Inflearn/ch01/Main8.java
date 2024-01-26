4edb322811f6c3d0800845ad512cb4fc0c51767b
beautiflow <1114ny@naver.com>
2024-01-26 16:32:49 +0900

    [inflearn] 01-08

---
+++ src/Inflearn/ch01/Main8.java
@@ -0,0 +1,32 @@
+package Inflearn.ch01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main8 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine().replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]", "");
+        String changeStr = str.toLowerCase();
+        int count = 0;
+
+        char[] arr = changeStr.toCharArray();
+
+        for(int i = 0; i < str.length()/2; i++){
+            if(arr[i] == arr[str.length()-1-i]){
+                count++;
+            }
+        }
+
+        if(count == str.length()/2){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+
+}
