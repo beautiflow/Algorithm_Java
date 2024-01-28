e94896111dd5e1b9bbb6ff30f018a654c24e9e75
beautiflow <1114ny@naver.com>
2024-01-28 23:56:37 +0900

    name refactor

--- src/Inflearn/ch01/Main1.java
+++ src/Inflearn/ch01/Main1.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class FindWord {
+public class Main1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
