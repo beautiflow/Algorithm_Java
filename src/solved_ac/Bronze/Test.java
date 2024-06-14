0cbf68baef1c07a6bcb48225c74009757c0ca714
beautiflow <1114ny@naver.com>
2024-06-14 23:58:17 +0900

    test

---
+++ src/solved_ac/Bronze/Test.java
@@ -0,0 +1,35 @@
+package solved_ac.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int light = Integer.parseInt(st.nextToken());
+        int row;
+        int height;
+        int menu;
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+             row = Integer.parseInt(st.nextToken());
+             height = Integer.parseInt(st.nextToken());
+             menu = Integer.parseInt(st.nextToken());
+        }
+
+        // row 기준 정렬
+
+
+
+
+
+    }
+
+
+}
